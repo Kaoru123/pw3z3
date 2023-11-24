b9964c043d333e55119ffabe42ea61ff753221ac
flora <flora@DESKTOP-P1SPOFP>
2023-11-24 20:56:41 +0300

    Добавьте файлы проекта.

---
+++ pw3z3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class pw3z3
+{
+        static void Main(string[] args)
+        {
+            SQLCommand command = new SQLCommand();
+            Console.WriteLine("Введите SQL-запрос:");
+            command.CommandText = Console.ReadLine();
+            Console.WriteLine($"Введенный SQL-запрос (в верхнем регистре): {command.CommandText}");
+        }
+    }
